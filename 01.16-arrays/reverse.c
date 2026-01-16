cfe4845c89e368a42062096540b5e53d441210f7
Christopher E. Siu <cesiu@calpoly.edu>
2026-01-16 10:02:09 -0800

    Added examples of arrays as pointers.

--- 01.16-arrays/reverse.c
+++ 01.16-arrays/reverse.c
@@ -22,7 +22,24 @@ void swap(int *x, int *y) {
     *y = temp;
 }
 
+/* NOTE: In C, arguments are always pass-by-value. The value of an array is the
+ *       address of its first element, thus, when an array is passed into a
+ *       function, it "decays" into a pointer. Note the length must be passed
+ *       separately if needed. */
 void reverse(int arr[], int len) {
-    (void)arr;
-    (void)len;
+    int i;
+
+    for (i = 0; i < len / 2; i++) {
+        swap(&arr[i], &arr[len - 1 - i]);
+
+        /* NOTE: The array and pointer syntaxes are largely interchangeable;
+         *       indexing an array is offsetting and dereferencing its address,
+         *       so the above is equivalent to the following: */
+        swap(arr + i, arr + (len - 1 - i));
+    }
+
+    /* NOTE: A NULL pointer is guaranteed to be an invalid address for data, so
+     *       this will cause a "segfault" when the swap function tries to use
+     *       those invalid addresses.
+     * swap(NULL, NULL); */
 }
