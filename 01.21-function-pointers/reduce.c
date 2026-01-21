b2c7ecb682479c16562ebec4e7ee1b1ce7d74d65
Christopher E. Siu <cesiu@calpoly.edu>
2026-01-21 11:57:51 -0800

    Added examples of function pointers.

--- 01.21-function-pointers/reduce.c
+++ 01.21-function-pointers/reduce.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
+#include "add.h"
+#include "max.h"
 
-int reduce(int[], int);
+int reduce(int[], int, int (*)(int, int));
 
 int main(void) {
     int arr[] = {2, -1, 9, 8, 5, -3, 0, 8};
 
-    printf("reduce(arr, 8): %d\n", reduce(arr, 8));
+    printf("reduce(%p, 8, %p): %d\n",
+     (void *)arr, (void *)add, reduce(arr, 8, add));
+
+    printf("reduce(%p, 8, %p): %d\n",
+     (void *)arr, (void *)max, reduce(arr, 8, max));
 
     return 0;
 }
 
-int reduce(int arr[], int n) {
+/* NOTE: Finding the sum is very similar to finding the max element: both
+ *       involve repeatedly combining two elements in some way. To avoid
+ *       duplicating code, we can pass a pointer to a function that does that
+ *       combining operation. */
+int reduce(int arr[], int n, int (*fn)(int, int)) {
     int val = arr[0], i;
 
     for (i = 1; i < n; i++) {
-        val = val + arr[i];
+        val = (*fn)(val, arr[i]);
     }
 
     return val;
