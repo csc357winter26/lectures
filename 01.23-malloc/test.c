545f23f619f92381ee84cbe51a445f2bffcae71f
Christopher Siu <christopher@siu.sh>
2026-01-23 10:50:50 -0800

    Added example of malloc.

--- 01.23-malloc/test.c
+++ 01.23-malloc/test.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int *pair(int, int);
@@ -14,7 +15,15 @@ int main(void) {
 }
 
 int *pair(int first, int second) {
-    int arr[2];
+    /* NOTE: This is never safe. Automatically allocated locals on the runtime
+     *       stack must be deallocated on return, so this will end up returning
+     *       a pointer to memory that is no longer in use.
+     * int arr[2]; */
+
+    /* NOTE: Instead, dynamically allocated data on the heap can be allocated
+     *       at runtime and will not be deallocated on return. Recall that
+     *       we are allowed to use array syntax on pointers. */
+    int *arr = (int *)malloc(sizeof(int) * 2);
 
     arr[0] = first;
     arr[1] = second;
