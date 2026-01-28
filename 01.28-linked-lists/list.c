dd1736eaf702e9c0d7f3d7610502d843b09bac72
cesiu <cesiu@calpoly.edu>
2026-01-28 10:07:35 -0800

    Added example of linked lists.

--- 01.28-linked-lists/list.c
+++ 01.28-linked-lists/list.c
@@ -3,11 +3,25 @@
 
 /* lstcreate: Creates an empty linked list. */
 List *lstcreate() {
-    return NULL;
+    List *lst = (List *)malloc(sizeof(List));
+
+    lst->head = NULL;
+    lst->size = 0;
+
+    return lst;
 }
 
 /* lstdestroy: Destroys an existing linked list. */
 void lstdestroy(List *lst) {
+    Node *cur = lst->head;
+
+    while (cur != NULL) {
+        Node *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+
+    free(lst);
 }
 
 /* lstget: Gets an element in a linked list. */
@@ -22,6 +36,29 @@ int lstset(List *lst, int idx, void *val) {
 
 /* lstadd: Adds an element to a linked list. */
 int lstadd(List *lst, int idx, void *val) {
+    Node *node = (Node *)malloc(sizeof(Node));
+
+    node->val = val;
+    node->next = NULL;
+
+    if (idx == 0) {
+        node->next = lst->head;
+        lst->head = node;
+    }
+    else {
+        Node *cur = lst->head;
+        int i;
+
+        for (i = 0; i < idx - 1; i++) {
+            cur = cur->next;
+        }
+
+        node->next = cur->next;
+        cur->next = node;
+    }
+
+    (lst->size)++;
+
     return 0;
 }
 
