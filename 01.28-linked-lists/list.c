6c13d453a00dcc52cad0b66bbb1f0cace1771b1d
cesiu <cesiu@calpoly.edu>
2026-01-28 12:24:03 -0800

    Added example of linked lists.

--- 01.28-linked-lists/list.c
+++ 01.28-linked-lists/list.c
@@ -3,11 +3,23 @@
 
 /* lstcreate: Creates an empty linked list. */
 List *lstcreate() {
-    return NULL;
+    List *lst = (List *)malloc(sizeof(List));
+
+    lst->head = NULL;
+    lst->size = 0;
+
+    return lst;
 }
 
 /* lstdestroy: Destroys an existing linked list. */
 void lstdestroy(List *lst) {
+    while (lst->head != NULL) {
+        Node *next = lst->head->next;
+        free(lst->head);
+        lst->head = next;
+    }
+
+    free(lst);
 }
 
 /* lstget: Gets an element in a linked list. */
@@ -22,6 +34,31 @@ int lstset(List *lst, int idx, void *val) {
 
 /* lstadd: Adds an element to a linked list. */
 int lstadd(List *lst, int idx, void *val) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->val = val;
+    node->next = NULL;
+
+    if (idx == 0) {
+        node->next = lst->head;
+        lst->head = node;
+    }
+    else {
+        Node *prev = lst->head;
+        int i;
+
+        for (i = 0; i < idx - 1; i++) {
+            prev = prev->next;
+        }
+
+        node->next = prev->next;
+        prev->next = node;
+    }
+
+    (lst->size)++;
+
+    /* NOTE: This function returns an error code. If, for example, it was
+     *       possible for idx to be out-of-bounds, then we could check for that
+     *       and potentially return some non-zero number instead. */
     return 0;
 }
 
