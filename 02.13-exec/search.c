0245048eaf70a50c798cf2f2ac6542552c15826d
cesiu <cesiu@calpoly.edu>
2026-02-13 12:26:39 -0800

    Added example of exec.

--- 02.13-exec/search.c
+++ 02.13-exec/search.c
@@ -7,15 +7,30 @@
 
 #define LINELEN 81
 
-int fsearch(char *, char *);
-
 int main(int argc, char *argv[]) {
     int i, tmp, status = EXIT_FAILURE;
     pid_t pid;
 
     for (i = 2; i < argc; i++) {
         if ((pid = fork()) == 0) {
-            exit(fsearch(argv[1], argv[i]));
+            /* NOTE: There already exists a program, "grep", for searching
+             *       files; there is no reason to rewrite that code -- there is
+             *       certainly no reason to write that code in the same program
+             *       as the parent process that will never need it.
+             * exit(fsearch(argv[1], argv[i])); */
+
+            /* NOTE: In this case, we need "execLP"; we are going to pass a
+             *       List of arguments, and we need the OS to search the Path
+             *       for the "grep" executable. By convention, the first
+             *       argument is always the name of the executable. */
+            execlp("grep", "grep", argv[1], argv[i], NULL);
+
+            /* NOTE: Once we call "exec", there is no coming back, since the
+             *       current executable will have been replaced. The only way
+             *       to get this far is if the call failed. This also means we
+             *       can't replace the parent; it has other responsibilities. */
+            perror("execlp");
+            exit(EXIT_FAILURE);
         }
     }
 
@@ -26,22 +41,5 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    return status;
-}
-
-/* fsearch: Prints occurrences of a string in a file. */
-int fsearch(char *str, char *fname) {
-    int status = EXIT_FAILURE;
-    char buf[LINELEN];
-    FILE *file = fopen(fname, "r");
-
-    while (fgets(buf, LINELEN, file) != NULL) {
-        if (strstr(buf, str) != NULL) {
-            printf("%s: %s", fname, buf);
-            status = EXIT_SUCCESS;
-        }
-    }
-
-    fclose(file);
     return status;
 }
