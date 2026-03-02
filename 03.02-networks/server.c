13a0e5dec00a7f082838da3962a1b2bd5f055b02
cesiu <cesiu@calpoly.edu>
2026-03-02 09:54:37 -0800

    Added example of getaddrinfo.

--- 03.02-networks/server.c
+++ 03.02-networks/server.c
@@ -2,5 +2,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    /* NOTE: This program, the server, will already be running and will
+     *       passively wait for other programs, the clients, to connect. */
+
     return EXIT_SUCCESS;
 }
