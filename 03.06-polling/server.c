396d35e313e320048889819430f44170f2c91068
cesiu <cesiu@calpoly.edu>
2026-03-06 11:59:24 -0800

    Added example of non-blocking I/O.

--- 03.06-polling/server.c
+++ 03.06-polling/server.c
@@ -1,13 +1,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
 #include <netdb.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints = {0}, *addr;
-    int fd, client, n;
+    struct pollfd clients[17];
+    int fd, n, m, i;
     char buf[80];
 
     /* NOTE: This program (the server) will already be running, passively
@@ -21,14 +25,57 @@ int main(int argc, char *argv[]) {
     getaddrinfo(NULL, argv[1], &hints, &addr);
     fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
     bind(fd, addr->ai_addr, addr->ai_addrlen);
-    listen(fd, 1);
+    listen(fd, 16);
 
-    client = accept(fd, NULL, NULL);
-    while ((n = read(client, buf, 80)) > 0) {
-        write(STDOUT_FILENO, buf, n);
+    /* NOTE: For simplicity, we'll assume that we only need to handle 16
+     *       clients, which requires 17 descriptors: the 16 clients and the
+     *       1 original listening socket. */
+
+    clients[0].fd = fd;
+    clients[0].events = POLLIN;
+    fcntl(clients[0].fd, F_SETFL, O_NONBLOCK);
+    m = 1;
+
+    while (poll(clients, m, m > 1 ? -1 : 10000) > 0) {
+        /* NOTE: Since it is generally possible that some file descriptors are
+         *       more important than others, "poll" only tells us how many are
+         *       ready for I/O, rather than which ones are ready for I/O. */
+        for (i = 0; i < m; i++) {
+            if (clients[i].revents & POLLIN) {
+                if (i == 0) {
+
+                    /* NOTE: If the listening socket has data available, we'll
+                     *       accept a new client and add that client's socket
+                     *       to the array of polled descriptors. */
+                    clients[m].fd = accept(fd, NULL, NULL);
+                    clients[m].events = POLLIN;
+                    fcntl(clients[m].fd, F_SETFL, O_NONBLOCK);
+                    m++;
+                }
+                else {
+
+                    /* NOTE: If a client socket has data availabe, we'll read
+                     *       that data and echo it to the terminal. With the
+                     *       client socket set to non-blocking... */
+
+                    while ((n = read(clients[i].fd, buf, 80)) > 0) {
+                        write(STDOUT_FILENO, buf, n);
+                    }
+
+                    /* NOTE: ...there are two ways to break the above loop:
+                     *       the client has closed the connection or there is
+                     *       no more data available yet. */
+
+                    if (n == 0 || errno != EAGAIN) {
+                        close(clients[i].fd);
+                        clients[i] = clients[m - 1];
+                        m--;
+                    }
+                }
+            }
+        }
     }
 
-    close(client);
     close(fd);
     freeaddrinfo(addr);
 
